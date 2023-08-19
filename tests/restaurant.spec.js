76baba203a29d7ce38515e0f05e59153e31d6305
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-19 16:28:01 -0300

    Criando os primeiros testes

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,13 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+    expect(createMenu()).toMatchObject(fetchMenu);
+    expect(typeof createMenu()).toEqual('function');
   });
+  it('Verifica se a função `createMenu` retorna um objeto com as chaves `food` e `drinks`', () => {
+    expect(createMenu()).toContainEqual('food', 'drinks');
+  })
+  it('Verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu', () => {
+    expect(createMenu()).toContainEqual({food: {},drinks: {},});
+  })
 });
