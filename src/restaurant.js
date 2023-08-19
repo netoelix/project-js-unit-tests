9a78d3a1b3d3e152a8962dad397c733678f926f3
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-19 17:45:22 -0300

    Refatorando o codigo, corrigindo testes que não funcionavam

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,14 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (menuObj) => {
+  const order = [];
+  const menu = {
+    fetchMenu: () => menuObj,
+    consumption: order,
+  };
+  return menu;
+};
+console.log(createMenu({ food: {}, drinks: {}, }));
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,14 +1,23 @@
 const createMenu = require('../src/restaurant');
- 
+
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    expect(createMenu()).toMatchObject(fetchMenu);
-    expect(typeof createMenu()).toEqual('function');
+    const newMenu = createMenu({ food: {}, drinks: {}, });
+
+    //Verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu.
+    expect(newMenu).toHaveProperty('fetchMenu');
+
+    //Verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função.
+    expect(typeof createMenu().fetchMenu).toBe('function');
+    const menuNewObj = { food: {}, drinks: {}, };
+    const menuInstance = createMenu(menuNewObj);
+    //verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.
+    expect(menuInstance.fetchMenu()).toHaveProperty('food');
+    expect(menuInstance.fetchMenu()).toHaveProperty('drinks');
+    
+    //Verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().
+    expect(newMenu).toEqual(newMenu);
+
+    expect(createMenu().consumption).toStrictEqual([]);
   });
-  it('Verifica se a função `createMenu` retorna um objeto com as chaves `food` e `drinks`', () => {
-    expect(createMenu()).toContainEqual('food', 'drinks');
-  })
-  it('Verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu', () => {
-    expect(createMenu()).toContainEqual({food: {},drinks: {},});
-  })
 });
