6865956c40af2b95853a21eb108d1605d46bae3d
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-19 14:59:11 -0300

    Criando as funções Calculator e ArrayGenerator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let newArray;
+  if (type === 'keys') {
+    newArray = Object.keys(object);
+    console.log(newArray);
+  } else if (type === 'values') {
+    newArray = Object.values(object);
+    console.log(newArray);
+  } else {
+    newArray = Object.entries(object);
+    console.log(newArray);
+  }
+  return newArray;
+};
 
 module.exports = { calculator, arrayGenerator };
